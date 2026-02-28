6ec007feec1ef5e388a71739ce07878d1881a684
A4310 <teguh.kusuma@berijalan.co.id>
2026-02-28 12:29:16 +0700

    add: initial identity schema and setup for PersonalFinanceWeb Mini Project

---
+++ PersonalFinanceWeb/PersonalFinanceWeb/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace PersonalFinanceWeb.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ PersonalFinanceWeb/PersonalFinanceWeb/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace PersonalFinanceWeb.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
